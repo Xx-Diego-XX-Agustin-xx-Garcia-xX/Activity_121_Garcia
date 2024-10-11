2fb6e460b7008d9cb6b8af3af959b2ff12131545
Xx-Diego-XX-Agustin-xx-Garcia-xX <116301540+Xx-Diego-XX-Agustin-xx-Garcia-xX@users.noreply.github.com>
2024-10-11 11:41:21 -0700

    Updated leaderboard.py
    Completed TODO #1

--- leaderboard.py
+++ leaderboard.py
@@ -1,5 +1,4 @@
 # leaderboard.py
-# The leaderboard module to be used in Activity 1.2.2
 # set the levels of scoring
 bronze_score = 15
 silver_score = 20
@@ -7,15 +6,16 @@ gold_score = 25
 
 # return names in the leaderboard file
 def get_names(file_name):
-  leaderboard_file = open(file_name, "r")  # be sure you have created this
-  # use a for loop to iterate through the content of the file, one line at a time
+  leaderboard_file = open(file_name, "r")
   # note that each line in the file has the format "leader_name,leader_score" for example "Pat,50"
   names = []
   for line in leaderboard_file:
     leader_name = ""
     index = 0
     # TODO 1: use a while loop to read the leader name from the line (format is "leader_name,leader_score")
-    while 
+    while (line[index] != ","):
+      leader_name = leader_name + line[index] 
+      index = index + 1 
     # TODO 2: add the player name to the names list
   leaderboard_file.close()
   #  TODO 6: return the names list in place of the empty list
